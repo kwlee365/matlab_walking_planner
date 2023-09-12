668aad7a71ba142be8ceceb6e96e0201b1e76805
kwlee365 <kwlee365@snu.ac.kr>
2023-09-12 17:17:41 +0900

    modify CP MPC README.md

--- Krause_CP_MPC/BipedWalkingPlanner.m
+++ Krause_CP_MPC/BipedWalkingPlanner.m
@@ -21,7 +21,7 @@ step_time = 1.0;                % step time
 step_length = 2.0;
 step_width = 0.2;
 
-preview_window = 0.8*step_time*hz;
+preview_window = 2*step_time*hz;
 
 Qe = 1.0;            % preview gain
 Qx = zeros(3,3);     % preview gain
@@ -36,30 +36,30 @@ FootRef = [FootRef_x FootRef_y];
 [cp_ref, zmp_ref] = PreviewController(T,sim_tick,N,FootRef_x,FootRef_y,A,B,C,Gi,Gx,Gd,w,g,h);
 
 % Matrix manipulation
-A =    exp(w*T)  * eye(2);
-B = (1-exp(w*T)) * eye(2); 
+A =   exp(w*T);
+B = 1-exp(w*T); 
 
-F_xi = zeros(2*N, 2);
-F_p  = zeros(2*N, 2*N);
-Theta = zeros(2*N, 2*N);
-e1 = zeros(2*N, 2);
-e1(1:2,1:2) = eye(2);
+F_xi = zeros(N, 1);
+F_p  = zeros(N, N);
+Theta = zeros(N, N);
+e1 = zeros(N, 1);
+e1(1,1) = 1;
 
 for i = 1:1:N
 
-    F_xi(2*(i-1)+1:2*i, 1:2) = A^i;
-    Theta(2*(i-1)+1:2*i, 2*(i-1)+1:2*i) = eye(2);
+    F_xi(i, 1) = A^i;
+    Theta(i,i) = 1;
 
     for j = 1:1:N
         % (F_p)
         if(i < j)
-            F_p(2*(i-1)+1:2*i, 2*(j-1)+1:2*j) = zeros(2,2);
+            F_p(i,j) = 0;
         else
-            F_p(2*(i-1)+1:2*i, 2*(j-1)+1:2*j) = A^(i-j)*B;
+            F_p(i,j) = A^(i-j)*B;
         end
         % (Theta)
         if(i == j+1)
-            Theta(2*(i-1)+1:2*i, 2*(j-1)+1:2*j) = -eye(2);
+            Theta(i,j) = -1;
         end
     end
 end
@@ -68,84 +68,97 @@ current_step = 0;
 j = 1;
 
 %% MPC data
-cp_ref_mpc = zeros(2*sim_tick, 1);
-Foot_ref_mpc = zeros(2*sim_tick, 1);
-b_max = zeros(2*N, sim_tick);
-b_min = zeros(2*N, sim_tick);
+cp_x_ref_mpc = zeros(N, sim_tick);
+lb_x = zeros(N, sim_tick);
+ub_x = zeros(N, sim_tick);
 
+cp_y_ref_mpc = zeros(N, sim_tick);
+lb_y = zeros(N, sim_tick);
+ub_y = zeros(N, sim_tick);
+
+ 
 for i = 1:1:sim_tick
-    for j = 1:1:N
-        cp_ref_mpc(2*(j-1)+1,i) = cp_ref(i+j-1,1);
-        cp_ref_mpc(2*(j-1)+2,i) = cp_ref(i+j-1,2);  
-        
-        % x
-        b_max(2*(j-1)+1,i) = FootRef_x(i+j-1,1) + 0.16;
-        b_min(2*(j-1)+1,i) = - FootRef_x(i+j-1,1) + 0.14;
-        
-        % y
-        b_max(2*(j-1)+2,i) = FootRef_y(i+j-1,1) + 0.1;
-        b_min(2*(j-1)+2,i) = - FootRef_y(i+j-1,1) + 0.1;
-    end
+    cp_x_ref_mpc(1:N, i) = cp_ref(i:i+N-1, 1);
+    cp_y_ref_mpc(1:N, i) = cp_ref(i:i+N-1, 2);
+
+    lb_x(1:N, i) = FootRef_x(i:i+N-1, 1) - 1.3;
+    ub_x(1:N, i) = FootRef_x(i:i+N-1, 1) + 1.7;
+
+    lb_y(1:N, i) = FootRef_y(i:i+N-1, 1) - 1;
+    ub_y(1:N, i) = FootRef_y(i:i+N-1, 1) + 1;
 end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DSP is not considered in this planner. So, inequality conditions are  % 
+% dominant to plan the trajectories.                                    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Quadratic programming
 
-cp_hat = zeros(2,sim_tick); % real value  
-p_hat = zeros(2,sim_tick);
-cp_opt = zeros(2*N, sim_tick);  % opt value
-p_opt = zeros(2*N, sim_tick);
+cp_x_hat = zeros(1,sim_tick); % real value  
+p_x_hat = zeros(1,sim_tick);
+cp_x_opt = zeros(N, sim_tick);  % opt value
+p_x_opt = zeros(N, sim_tick);
+
+cp_y_hat = zeros(1,sim_tick); % real value  
+p_y_hat = zeros(1,sim_tick);
+cp_y_opt = zeros(N, sim_tick);  % opt value
+p_y_opt = zeros(N, sim_tick);
+
+Q = eye(N, N);
+R = 0.00001 * eye(N, N);
 
 for k=1:1:sim_tick
 
     % Cost function
-    Q = eye(2*N, 2*N);
-    R = 0.1 * eye(2*N, 2*N);
-    H = Theta' * R * Theta + F_p' * Q * F_p;
+
+    H = transpose(Theta) * R * Theta + transpose(F_p) * Q * F_p;
+
     if k == 1
-        g = F_p' * Q * (F_xi * cp_hat(1:2, k) - cp_ref_mpc(1:2*N,k)) - Theta' * R * e1 * p_hat(1:2,k);
+        gx = transpose(F_p) * Q * (F_xi * cp_x_hat(1, k) - cp_x_ref_mpc(1:N,k)) - transpose(Theta) * R * e1 * p_x_hat(1,k);
+        gy = transpose(F_p) * Q * (F_xi * cp_y_hat(1, k) - cp_y_ref_mpc(1:N,k)) - transpose(Theta) * R * e1 * p_y_hat(1,k);
     else
-        g = F_p' * Q * (F_xi * cp_hat(1:2, k) - cp_ref_mpc(1:2*N,k)) - Theta' * R * e1 * p_hat(1:2,k-1);
+        gx = transpose(F_p) * Q * (F_xi * cp_x_hat(1, k) - cp_x_ref_mpc(1:N,k)) - transpose(Theta) * R * e1 * p_x_hat(1,k-1);
+        gy = transpose(F_p) * Q * (F_xi * cp_y_hat(1, k) - cp_y_ref_mpc(1:N,k)) - transpose(Theta) * R * e1 * p_y_hat(1,k-1);
     end
 
-    % Constraints
-    Amax = eye(2*N, 2*N);
-    Amin = -1.0 * eye(2*N, 2*N);
+    px_opt(1:N,k) = quadprog(H, gx,[],[],[],[], lb_x(1:N,k), ub_x(1:N,k));
+    py_opt(1:N,k) = quadprog(H, gy,[],[],[],[], lb_y(1:N,k), ub_y(1:N,k));
 
-    Aconst = [Amax;
-              Amin];
+    % cp_x_opt(1:N,k) = F_xi * cp_x_hat(1, k) + F_p * px_opt(1:N,k);
+    % cp_y_opt(1:N,k) = F_xi * cp_x_hat(1, k) + F_p * py_opt(1:N,k);
 
-    bconst(1:4*N,k) = [b_max(1:2*N,k);
-                       b_min(1:2*N,k)];
+    px_hat(1,k) = px_opt(1,k);
+    py_hat(1,k) = py_opt(1,k);
 
-    p_opt(1:2*N,k) = quadprog(H, g, Aconst, bconst(1,4*N,k));
-    % p_opt(1:2*N,k) = quadprog(H, g,[],[],[],[], b_min(1:2*N,k), b_max(1:2*N,k));
-    cp_opt(1:2*N,k+1) = F_xi * cp_hat(1:2, k) + F_p * p_opt(1:2*N,k);
-    
-    p_hat(1:2,k) = p_opt(1:2,k);
-    cp_hat(1:2,k+1) = A*cp_hat(1:2,k) + B*p_hat(1:2,k);
+    cp_x_hat(1,k+1) = cp_x_opt(1,k);
+    cp_y_hat(1,k+1) = cp_y_opt(1,k);
+
+    cp_x_hat(1,k+1) = A*cp_x_hat(1,k) + B*px_hat(1,k);
+    cp_y_hat(1,k+1) = A*cp_y_hat(1,k) + B*py_hat(1,k);
 end
 
 %%
 figure()
-plot([1:sim_tick], cp_ref(1:sim_tick,1), [1:sim_tick], cp_hat(1,1:sim_tick)')
+plot([1:sim_tick], cp_ref(1:sim_tick,1), [1:sim_tick], cp_x_hat(1,1:sim_tick)')
 legend('cp ref', 'cp mpc')
 title('x')
 ylim([-2 10])
 
 figure()
-plot([1:sim_tick], cp_ref(1:sim_tick,2), [1:sim_tick], cp_hat(2,1:sim_tick)')
+plot([1:sim_tick], cp_ref(1:sim_tick,2), [1:sim_tick], cp_y_hat(1,1:sim_tick)')
 legend('cp ref', 'cp mpc')
 title('y')
 ylim([-2 2])
 
 figure()
-plot([1:sim_tick], FootRef_x(1:sim_tick,1), [1:sim_tick], p_hat(1,1:sim_tick)')
+plot([1:sim_tick], FootRef_x(1:sim_tick,1), [1:sim_tick], px_hat(1,1:sim_tick)')
 legend('p ref', 'p mpc')
 title('x')
 ylim([-2 10])
 
 figure()
-plot([1:sim_tick], FootRef_y(1:sim_tick,1), [1:sim_tick], p_hat(2,1:sim_tick)')
+plot([1:sim_tick], FootRef_y(1:sim_tick,1), [1:sim_tick], py_hat(1,1:sim_tick)')
 legend('p ref', 'p mpc')
 title('y')
 ylim([-2 2])
--- Krause_CP_MPC/README.md
+++ Krause_CP_MPC/README.md
@@ -1,3 +1,3 @@
 # Reference
 
-Englsberger, Johannes, Christian Ott, and Alin Albu-Schäffer. "Three-dimensional bipedal walking control based on divergent component of motion." Ieee transactions on robotics 31.2 (2015): 355-368.
+Krause, Manuel, et al. "Stabilization of the capture point dynamics for bipedal walking based on model predictive control." IFAC Proceedings Volumes 45.22 (2012): 165-171.
